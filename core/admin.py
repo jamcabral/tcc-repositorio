500b6bb585026cadaad4d902903725ed1e56d0e1
Jammesson <jammessoncabral@gmail.com>
2020-02-14 14:24:21 -0300

    Ajustes Django Admin

--- core/admin.py
+++ core/admin.py
@@ -1,18 +1,51 @@
 from django.contrib import admin
-from .models import Curso, Turma, Professor, Coordenador, Aluno, Sala, Tcc, AppTcc
+from .models import Curso, Turma, Professor, Coordenador, Aluno, Sala, Tcc, Defesa
 
 # Register your models here.
 admin.site.site_header = "TCC SISTEMA"
 admin.site.site_title = "TCC SISTEMA"
 admin.site.index_title = "TCC SISTEMA"
 
-admin.site.register(Curso)
 admin.site.register(Turma)
 admin.site.register(Professor)
 admin.site.register(Coordenador)
 admin.site.register(Aluno)
 admin.site.register(Sala)
 admin.site.register(Tcc)
-admin.site.register(AppTcc)
 
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('id_curso', 'nome_curso')
+    search_fields = ['id_curso', 'nome_curso']
+
+"""
+class radiologia_examesAdmin(ModelAdmin):
+    list_display = ('nome_paciente_paciente', 'radilogia_categoria', 'dt_solicitacao_exame', 'status_radiologia_exame', 'nome_tec_radiologia', 'anexo_radiologia_exame',
+     'anexo2_radiologia_exame', 'anexo3_radiologia_exame', 'anexo4_radiologia_exame', 'anexo5_radiologia_exame',)
+    search_fields = ['nome_paciente_paciente__nome_paciente', 'nome_paciente_paciente__cpf_paciente', 'status_radiologia_exame','nome_paciente_paciente__dt_nascimento_paciente']
+    autocomplete_fields = ['radilogia_categoria', 'nome_paciente_paciente', 'nome_tec_radiologia']
+    fieldsets = (
+        ('AREA DE SOLICITAÇÃO DE EXAMES (MÉDICOS)', {
+            'fields': ('nome_paciente_paciente', 'dt_nascimento_exame', 'radilogia_categoria', 'obs_radiologia_exame')
+        }),
+        ('AREA PARA EXECUÇÃO DE EXAMES (TEC. RADIOLOGIA)', {
+            'classes': ('collapse',),
+            'fields': ('anexo_radiologia_exame', 'anexo2_radiologia_exame', 'anexo3_radiologia_exame', 'anexo4_radiologia_exame', 'anexo5_radiologia_exame',
+            'status_radiologia_exame', 'nome_tec_radiologia','dt_realizado_exame',),
+        }),
+    )
+    
+
+class radiologia_categoriaAdmin(ModelAdmin):
+    search_fields = ['nome_radiologia_categoria']
+    list_display = ('nome_radiologia_categoria', 'id_radiologia_categoria',)
+"""
+
+class DefesaAdmin(admin.ModelAdmin):
+    list_display = ('id_tcc_df', 'id_sala_df', 'dt_df', 'banca1_df', 'banca2_df')
+    search_fields = ['id_tcc_df', 'id_sala_df', 'dt_df', 'banca1_df', 'banca2_df']
+
+
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(Defesa, DefesaAdmin)
 
--- core/models.py
+++ core/models.py
@@ -15,6 +15,10 @@ class Curso(models.Model):
     nome_curso = models.CharField(verbose_name="Nome do Curso", max_length=50)
     def __str__(self):
         return self.nome_curso
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
 
 class Turma(models.Model):
     id_turma = models.AutoField(primary_key=True)
@@ -23,6 +27,10 @@ class Turma(models.Model):
     periodo_turma = models.IntegerField(verbose_name="Periodo Turma")
     def __str__(self):
         return self.nome_turma
+    class Meta:
+        verbose_name = 'Turma'
+        verbose_name_plural = 'Turmas'
+
 class Professor(models.Model):
     class CONDICAO_CHOICE(models.TextChoices):
         INTERNO = 'INTERNO', _('INTERNO'),
@@ -34,6 +42,9 @@ class Professor(models.Model):
     condicao_professor = models.CharField(verbose_name="Externo ou Interno", max_length=50, choices=CONDICAO_CHOICE.choices, default=CONDICAO_CHOICE.INTERNO)
     def __str__(self):
         return self.nome_professor
+    class Meta:
+        verbose_name = 'Professor'
+        verbose_name_plural = 'Professores'
 
 class Coordenador(models.Model):
     id_coordenador = models.AutoField(verbose_name="Identificador Coornedaor", primary_key=True)
@@ -41,13 +52,20 @@ class Coordenador(models.Model):
     id_curso_coordenador = models.ForeignKey(Curso, verbose_name="Curso", on_delete=models.CASCADE)
     def __str__(self):
         return self.nome_coordenador
+    class Meta:
+        verbose_name = 'Coordenador'
+        verbose_name_plural = 'Coordenadores'
+
+
 class Aluno(models.Model):
     mat_aluno = models.AutoField(verbose_name="Matricula Aluno", primary_key=True)
     nome_aluno = models.CharField(verbose_name="Nome Aluno", max_length=50)
     id_curso_aluno = models.ForeignKey(Curso, verbose_name="Curso", on_delete=models.CASCADE)
     def __str__(self):
-        return  'NOME: ' + self.mat_aluno + " - " + self.nome_aluno
-
+        return  self.nome_aluno
+    class Meta:
+        verbose_name = 'Aluno'
+        verbose_name_plural = 'Alunos'
 
 class Sala(models.Model):
     id_sala = models.AutoField(primary_key=True)
@@ -55,6 +73,9 @@ class Sala(models.Model):
     localizacao_sala = models.CharField(verbose_name="Localização da Sala", max_length=50)
     def __str__(self):
         return self.nome_sala
+    class Meta:
+        verbose_name = 'Sala'
+        verbose_name_plural = 'Salas'
 
 
 class Tcc(models.Model):
@@ -65,19 +86,25 @@ class Tcc(models.Model):
     id_curso_tcc = models.ForeignKey(Curso, related_name='NomeCurso', on_delete=models.CASCADE)
     orientador_tcc = models.ForeignKey(Professor, related_name='Orientador', on_delete=models.CASCADE)
     co_orientador_tcc = models.ForeignKey(Professor, related_name='Coorientador', on_delete=models.CASCADE)
-    banca1_tcc = models.ForeignKey(Professor, related_name='Bancaexaminadora', on_delete=models.CASCADE)
-    banca2_tcc = models.ForeignKey(Professor, related_name='Bancae_xaminadora', on_delete=models.CASCADE)
     carta_aceite_tcc = models.FileField(verbose_name="CartadeAceita", upload_to=tcc_directory_path, blank=True, null=True)
     convite_banca_tcc = models.FileField(verbose_name="ConviteBanca", upload_to=tcc_directory_path , blank=True, null=True)
     marcacao_banca_tcc = models.FileField(verbose_name="MarcaçãoBanca", upload_to=tcc_directory_path, blank=True, null=True)
     def __str__(self):
-        return self.tema_tcc + " - " + self.id_aluno_tcc
+        return 'TEMA: ' + self.tema_tcc + '  |   Nome: ' + self.id_aluno_tcc.nome_aluno
+    class Meta:
+        verbose_name = 'Tcc'
+        verbose_name_plural = 'Tccs'
     
 
-class AppTcc(models.Model):
-    id_apptcc = models.AutoField(verbose_name="Identificador Apresentacao", primary_key=True)
-    id_tcc_apptcc = models.ForeignKey(Tcc, related_name='TemaApresentacao', on_delete=models.CASCADE)
-    id_sala_apptcc = models.ForeignKey(Sala, related_name='SaladaApresentacao', on_delete=models.CASCADE)
-    dt_apptcc = models.DateTimeField(verbose_name="Data e Hora da Apresentacao", auto_now=False, auto_now_add=False)
+class Defesa(models.Model):
+    id_df = models.AutoField(verbose_name="Identificador Apresentacao", primary_key=True)
+    id_tcc_df = models.ForeignKey(Tcc, related_name='TemaApresentacao', on_delete=models.CASCADE)
+    id_sala_df = models.ForeignKey(Sala, related_name='SaladaApresentacao', on_delete=models.CASCADE)
+    dt_df = models.DateTimeField(verbose_name="Data e Hora da Apresentacao", auto_now=False, auto_now_add=False)
+    banca1_df = models.ForeignKey(Professor, related_name='Bancaexaminadora_defesa', on_delete=models.CASCADE)
+    banca2_df = models.ForeignKey(Professor, related_name='Bancae_xaminadora_defesa', on_delete=models.CASCADE)
     def __str__(self):
-        return self.id_sala_apptcc + " - " + self.dt_apptcc
+        return self.id_sala_df.nome_sala
+    class Meta:
+        verbose_name = 'Defesa'
+        verbose_name_plural = 'Defesas'

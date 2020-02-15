136b1cdf21dded4ef4ccc980429b49dd64b2c62f
Jammesson <jammessoncabral@gmail.com>
2020-02-15 14:42:15 -0300

    Crete Pagina TCC

--- core/views.py
+++ core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Turma
 
 
 def HomeView(request):
@@ -7,4 +8,6 @@ def HomeView(request):
 
 
 def TccView(request):
-    return render(request, 'tcc.html')
+    data = {}
+    data['turma'] = Turma.objects.all()
+    return render(request, 'tcc.html', data)

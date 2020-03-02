a064b987f161852aa77a068f9cfd37ead7f0a086
Jammesson <jammessoncabral@gmail.com>
2020-03-02 12:59:19 -0300

    Removando Venv Git

--- venvtcc/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/jammesson/PycharmProjects/tcc-repositorio/tcc-repositorio/venvtcc/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
